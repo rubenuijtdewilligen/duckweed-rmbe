c7ce13ec6a55b2ec2b2a5ba408905a3320b71dc0
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2026-06-16 16:19:30 +0200

    Bleaching score goes to max 4

--- src/statistics/main_analysis.R
+++ src/statistics/main_analysis.R
@@ -66,10 +66,10 @@ plot_bleaching <- ggplot(df_bleach_summary, aes(x = days_since_start, y = Mean_B
   geom_errorbar(aes(ymin = Mean_Bleaching - SEM_Bleaching, ymax = Mean_Bleaching + SEM_Bleaching), width = 0.15) +
   scale_color_manual(values = c("Low light intensity" = "#74c476", "High light intensity" = "#238b45")) +
   scale_x_continuous(breaks = unique(df_duckweed$days_since_start)) +
-  scale_y_continuous(limits = c(0, 5)) +
+  scale_y_continuous(limits = c(0, 4)) +
   labs(
     title = "Duckweed Bleaching Score Over Time",
-    subtitle = "Score: 0 (healthy) to 5 (completely bleached)",
+    subtitle = "Score: 0 (healthy) to 4 (completely bleached)",
     x = "Days Since Start",
     y = "Mean Bleaching Score",
     color = "Treatment"
